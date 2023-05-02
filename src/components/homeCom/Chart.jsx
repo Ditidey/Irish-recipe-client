32b8f029bc5bf77f61601b34b7338c87fab887fb
Ditidey <ditidey2017@gmail.com>
2023-05-02 23:39:11 +0100

    chart added

--- src/components/homeCom/Chart.jsx
+++ src/components/homeCom/Chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie } from 'recharts';
 
 const recipes = [
     {
@@ -39,52 +39,38 @@ const recipes = [
         "likes": 312
     }
 ]
-const gradientOffset = () => {
-    const dataMax = Math.max(recipes.map((i) => i.ratings));
-    const dataMin = Math.min(recipes.map((i) => i.ratings));
-  
-    if (dataMax <= 0) {
-      return 0;
-    }
-    if (dataMin >= 0) {
-      return 1;
-    }
-  
-    return dataMax / (dataMax - dataMin);
-  };
-  
-  const off = gradientOffset();
-  
+
+
 const Chart = () => {
     return (
-        <div className='md:mx-32 sm:mx-4 my-10'>
-        <p className='text-2xl font-bold text-center'>This char shows bests recipes and its popularity</p>
-        <ResponsiveContainer width="100%" height="100%">
-            <AreaChart
-                width={500}
-                height={400}
-                data={recipes}
-                margin={{
-                    top: 10,
-                    right: 30,
-                    left: 0,
-                    bottom: 0,
-                }}
-            >
-                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <defs>
-            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-              <stop offset={off} stopColor="green" stopOpacity={1} />
-              <stop offset={off} stopColor="red" stopOpacity={1} />
-            </linearGradient>
-          </defs>
-                <Area type="monotone" dataKey="ratings" stroke="#000" fill="url(#splitColor)" />
-            </AreaChart>
-        </ResponsiveContainer>
+    <div className='bg-orange-100 p-10'>
+        <p
+            className='text-3xl text-center font-bold mt-5'>This pie chart is showing best recipes
+        </p>
+
+        <div style={{ width: '100%', height: '100%' }} className='my-10 grid md:grid-cols-2 gap-5'>
+
+            <div style={{ width: '100%', height: 300 }}>
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie dataKey="ratings" data={recipes} outerRadius={120} fill="#8884d8" isAnimationActive={false} label />
+                        <Tooltip></Tooltip>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+
+            <div className='my-10 sm:mx-36'>
+                <p className='text-2xl font-bold'>6 Best Recipes</p>
+                <ol className='space-y-2 mx-4'>
+                    <li>Irish Stew</li>
+                    <li>Boxty</li>
+                    <li>Colcannon</li>
+                    <li>Bangers and Mash</li>
+                    <li>Shepherd's Pie</li>
+                </ol>
+            </div>
         </div>
+    </div>
     );
 };
 
